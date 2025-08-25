ff1d955c70ffd8c8bec45ebf3a288d5433433d1f
Lucas Fernandes <99408196+lucasfernandes27@users.noreply.github.com>
2025-08-25 14:53:27 -0300

    Add files via upload

---
+++ GerarDados.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import random
+from faker import Faker
+
+faker = Faker('pt_BR')
+
+dados_pessoas = []
+
+#cria até 10 dados fake
+for _ in range(10):
+    nome = faker.name()
+    cpf = faker.cpf()
+    idade = faker.random_int(min=18, max=60)
+    #organização da data
+    data = faker.date
+    endereco = faker.address()
+    estado = faker.city()
+    pais = 'Brasil'
+
+    pessoa = {
+        'nome': nome,
+        'cpf': cpf,
+        'idade': idade,
+        'endereco': endereco,
+        'estado': estado,
+        'pais': pais
+
+    }
+
+    #adicionar pessoas a lista
+    dados_pessoas.append(pessoa)
+
+#criara data frame
+df_pessoas = pd.DataFrame(dados_pessoas)
+print(df_pessoas)
